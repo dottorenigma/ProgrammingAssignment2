d176657f4c9f94670c0305d24d9df1f75d50a6a1
dottorenigma <antonio.perrone@gmail.com>
2015-02-18 16:48:49 +0100

    implemented alternative function makeCacheMatrix2()

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Matrix inversion is usually a costly computation and there may be some benefit to
 ## caching the inverse of a matrix rather than computing it repeatedly.
-## The pair of functions implemented here allows for caching the inverse of a matrix.
+## makeCacheMatrix() and cacheSolve() implemented here allow for caching the inverse of a matrix. 
+## They are a direct application of the pattern shown by the vectorMean
+## example to the matrix inverse case 
 
 ## makeCacheMatrix() creates an object that can cache a matrix and its inverse. 
 ## It returns a list of 4 functions: 
@@ -17,7 +19,7 @@ makeCacheMatrix <- function(x = matrix()) {
     inv <<- NULL
   }
   get <- function() x
-  setInv <- function(inverse) inv <<- inverse 
+  setInverse <- function(inverse) inv <<- inverse 
   getInv <- function() inv 
   list(set = set, get = get, setInv = setInv, getInv = getInv) 
 }
@@ -39,4 +41,36 @@ cacheSolve <- function(x, ...) {
   inv <- solve(data, ...)
   x$setInv(inv)
   inv
+}
+
+
+## makeCacheMatrix2() is an alternative implementation of the Matrix
+## inverse caching problem that does not need the cacheSolve() function.
+## makeCacheMatrix() creates an object that can cache a matrix and its inverse. 
+## It returns a list of 3 functions: 
+##
+## set() sets the value of the matrix
+## get() gets the value of the matrix
+## getInv() gets the value of the inverse matrix
+##
+## if a is invertible matrix and ca <- makeCacheMatrix2(a) the following 
+## invariant holds true
+## all(round(ca$getInv() %*% ca$get()) == diag(rep(1,nrow(ca$get()))))  
+
+makeCacheMatrix2 <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  getInv <- function(...) {
+    if(!is.null(inv)) {
+      message("getting cached inverse")
+      return(inv)
+    }
+    inv <<- solve(x, ...)
+    inv
+  }  
+  list(set = set, get = get, getInv = getInv) 
 }
